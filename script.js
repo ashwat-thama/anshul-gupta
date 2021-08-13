33b45ab46ea37012eedf6cada4799122cee58e6f
Anshul Gupta <66309571+ashwat-thama@users.noreply.github.com>
2021-08-13 22:54:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let item = document.querySelectorAll('.item');
+
+
+menu.onclick = () =>{
+  menu.classList.toggle('uil-times');
+  navbar.classList.toggle('active');
+}
+
+function linkAction (){
+    menu.classList.remove('uil-times');
+    navbar.classList.remove('active');
+}
+
+item.forEach(n => n.addEventListener('click' , linkAction));
+
+window.onscroll = () =>{
+  menu.classList.remove('uil-times');
+  navbar.classList.remove('active');
+}
+
+let AudioBtn = document.querySelector('#AudioBtn');
+let audio = document.getElementById('audio');
+AudioBtn.onclick = () =>{
+  let name = AudioBtn.classList[1]
+  if (name == 'uil-play-circle'){
+    AudioBtn.classList.remove(name);
+    AudioBtn.classList.add('uil-stop-circle');
+    AudioBtn.innerHTML = 'Stop';
+    audio.play();
+  }
+  if (name == 'uil-stop-circle'){
+    audio.pause();
+    AudioBtn.classList.remove('uil-stop-circle');
+    AudioBtn.classList.add('uil-play-circle');
+    AudioBtn.innerHTML = 'Play';
+  }
+}
